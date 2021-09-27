373f680eafaf1a4be4f112916a68f11a2b518edc
Nate Jones <nate@endot.org>
2021-09-26 17:04:52 -0700

    Switch to uberjar built-with tools.build for native image

---
+++ .build/build.clj
@@ -0,0 +1,15 @@
+(ns build
+  (:require
+    [clojure.tools.build.api :as b]
+    [org.corfield.build :as cb]))
+
+(def lib 'org.endot/tabl)
+(def version (format "1.0.%s" (b/git-count-revs nil)))
+
+(defn uber
+  "Build uberjar."
+  [opts]
+  (-> opts
+      (cb/clean)
+      (assoc :lib lib :version version :main 'app.main)
+      (cb/uber)))
